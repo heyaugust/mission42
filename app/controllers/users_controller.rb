49c2f4c3c65988a8c54c1fc2933e1d2d1cd18226
Aug <heyaugust@gmail.com>
2016-03-07 14:15:17 +0800

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		# flash = a temporary message 
   		flash[:success] = "Welcome to Mission 42 Online!"
   		redirect_to @user
