d495bf2da54b8f8d5220f43c2289ef6546d77e2f
Aug <heyaugust@gmail.com>
2016-03-09 12:40:55 +0800

    added stripe

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :charges
   # resources :auth_mailers this means that u need a auth mailer contoroller. that takes in a param of who to send it tp
 
    # The priority is based upon order of creation: first created -> highest priority.
